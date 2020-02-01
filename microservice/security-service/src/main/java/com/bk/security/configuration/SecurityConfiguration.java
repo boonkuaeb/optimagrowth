c3230bbe9e195f480f96582c15239b2e01334021
Boonkuae Boonsutta <boonkuae.boo@ascendcorp.com>
2020-02-01 20:59:54 +0700

    configure Spring Security Authentication

---
+++ microservice/security-service/src/main/java/com/bk/security/configuration/SecurityConfiguration.java
@@ -0,0 +1,30 @@
+package com.bk.security.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // Set configuration on the auth object
+        auth.inMemoryAuthentication()
+                .withUser("foo")
+                .password("bar")
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password("admin")
+                .roles("ADMIN")
+        ;
+    }
+
+    @Bean
+    public PasswordEncoder getPassword() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
